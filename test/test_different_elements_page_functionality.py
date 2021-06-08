e2793fb6b3477b36d97d8b89402381bf5ae6ca73
Stepan Petrosian <stephenpetrosian@gmail.com>
2021-06-08 13:57:39 +0300

    Divide test_different_elements_page_functionality in separate for each should check test cases

--- test/test_different_elements_page_functionality.py
+++ test/test_different_elements_page_functionality.py
@@ -6,29 +6,55 @@ from test.PageObjects.main_page import MainPage
 from test.constants import HOMEPAGE_URL
 
 
-def test_different_elements_page_functionality(user_credentials, browser):
-    # Open browser and go to page
-    main_page = MainPage(browser, HOMEPAGE_URL)
-    main_page.open()
+class TestDifferentElementsPageFunctionality:
+    def test_checkboxes(self, browser, user_credentials):
+        # Open browser and go to page
+        main_page = MainPage(browser, HOMEPAGE_URL)
+        main_page.open()
 
-    # Assert browser title
-    main_page.should_browser_title_be_correct()
+        # Perform login and check if user authorized
+        main_page.login(user_credentials["username"], user_credentials["password"])
+        main_page.should_be_authorised(user_credentials["full_username"])
 
-    # Perform login and check if user authorized
-    main_page.login(user_credentials["username"], user_credentials["password"])
-    main_page.should_be_authorised(user_credentials["full_username"])
+        # Open through the header menu Service -> Different Elements Page
+        main_page.open_service_sidebar_menu() \
+            .go_to_different_elements_page()
+        different_elements_page = DifferentElementsPage(browser=main_page.browser, url=main_page.browser.current_url)
 
-    # Open through the header menu Service -> Different Elements Page
-    main_page.open_service_sidebar_menu() \
-        .go_to_different_elements_page()
-    different_elements_page = DifferentElementsPage(browser=main_page.browser, url=main_page.browser.current_url)
+        # Select checkboxes. Assert there is a log for each checkbox
+        different_elements_page.select_checkbox(Checkboxes.WIND_CHECKBOX, "Wind")
+        different_elements_page.select_checkbox(Checkboxes.WATER_CHECKBOX, "Water")
 
-    # Select checkboxes. Assert there is a log for each checkbox
-    different_elements_page.select_checkbox(Checkboxes.WIND_CHECKBOX, "Wind")
-    different_elements_page.select_checkbox(Checkboxes.WATER_CHECKBOX, "Water")
+    def test_radiobutton(self, browser, user_credentials):
+        # Open browser and go to page
+        main_page = MainPage(browser, HOMEPAGE_URL)
+        main_page.open()
 
-    # Select radiobutton. Assert there is a log for radiobutton
-    different_elements_page.select_radiobutton(RadioButtons.SELEN_RADIOBUTTON, "Selen")
+        # Perform login and check if user authorized
+        main_page.login(user_credentials["username"], user_credentials["password"])
+        main_page.should_be_authorised(user_credentials["full_username"])
 
-    # Select in dropdown. Assert there is a log for dropdown
-    different_elements_page.select_element_from_dropdown(Dropdowns.ELEMENT_DROPDOWN, "Yellow")
+        # Open through the header menu Service -> Different Elements Page
+        main_page.open_service_sidebar_menu() \
+            .go_to_different_elements_page()
+        different_elements_page = DifferentElementsPage(browser=main_page.browser, url=main_page.browser.current_url)
+
+        # Select radiobutton. Assert there is a log for radiobutton
+        different_elements_page.select_radiobutton(RadioButtons.SELEN_RADIOBUTTON, "Selen")
+
+    def test_dropdown(self, browser, user_credentials):
+        # Open browser and go to page
+        main_page = MainPage(browser, HOMEPAGE_URL)
+        main_page.open()
+
+        # Perform login and check if user authorized
+        main_page.login(user_credentials["username"], user_credentials["password"])
+        main_page.should_be_authorised(user_credentials["full_username"])
+
+        # Open through the header menu Service -> Different Elements Page
+        main_page.open_service_sidebar_menu() \
+            .go_to_different_elements_page()
+        different_elements_page = DifferentElementsPage(browser=main_page.browser, url=main_page.browser.current_url)
+
+        # Select in dropdown. Assert there is a log for dropdown
+        different_elements_page.select_element_from_dropdown(Dropdowns.ELEMENT_DROPDOWN, "Yellow")
